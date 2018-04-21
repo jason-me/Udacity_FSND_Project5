f55595045e8c96c3d18f57812d092d6b975d8148
Jason Hester <JMH@Jasons-MacBook-Air.local>
2018-04-21 17:49:40 -0500

    Add new styles, update markerlist, animation, html

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var initLocations = [
-          {title: 'Old Settlers Park Disc Golf', position: {lat: 30.5410321, lng: -97.62581109999999}, FSID: '4c32511a7cc0c9b6871df09a'},
-          {title: 'Cat Hollow Disc Golf', position: {lat: 30.5064047, lng: -97.7304463}, FSID: '4c13eb38a9c220a17faa569d'},
-          {title: 'Falcon Pointe Disc Golf Course', position: {lat: 30.458378, lng: -97.5849549}, FSID: '4cafa62c1463a143934b96a9'},
-          {title: 'Wells Branch Disc Golf Course', position: {lat: 30.4342523, lng: -97.6712814}, FSID:'4ce72ffd8ef78cfa54b9919b'},
-          {title: 'Williamson County Disc Golf Course', position: {lat: 30.561017, lng: -97.7669112}, FSID: '4bc0eaf0461576b0880d7b32'},
-          {title: 'San Gabriel Disc Golf Course', position: {lat: 30.6332618, lng: -97.6779842}, FSID: '4aee1fb8f964a5204dd221e3'},
-          {title: 'Rivery Park Disc Golf Course', position: {lat: 30.5282057, lng: -97.6925576}, FSID: '4cb60ad764998cfa4d6912a2'},
-          {title: 'Bartholemews Disc Golf Course', position: {lat: 30.2967083, lng: -97.6895933}, FSID: '4f416feee4b0c868de8c7416'},
-          {title: 'MaryMoore Searight Metro Disc Golf Course', position: {lat: 30.2578349, lng: -97.7499692}, FSID: '4d7baad5ea35236a0ad34923'},
-          {title: 'Zilker Park Disc Golf Course', position: {lat: 30.2967083, lng: -97.8676655}, FSID: '4bbf8c4274a9a59378a4cef6'}
+          {category: '1', title: 'Old Settlers Park Disc Golf', position: {lat: 30.5410321, lng: -97.62581109999999}, FSID: '4c32511a7cc0c9b6871df09a'},
+          {category: '1', title: 'Cat Hollow Disc Golf', position: {lat: 30.5064047, lng: -97.7304463}, FSID: '4c13eb38a9c220a17faa569d'},
+          {category: '2', title: 'Falcon Pointe Disc Golf Course', position: {lat: 30.458378, lng: -97.5849549}, FSID: '4cafa62c1463a143934b96a9'},
+          {category: '2', title: 'Wells Branch Disc Golf Course', position: {lat: 30.4342523, lng: -97.6712814}, FSID:'4ce72ffd8ef78cfa54b9919b'},
+          {category: '3', title: 'Williamson County Disc Golf Course', position: {lat: 30.561017, lng: -97.7669112}, FSID: '4bc0eaf0461576b0880d7b32'},
+          {category: '3', title: 'San Gabriel Disc Golf Course', position: {lat: 30.6332618, lng: -97.6779842}, FSID: '4aee1fb8f964a5204dd221e3'},
+          {category: '4', title: 'Rivery Park Disc Golf Course', position: {lat: 30.5282057, lng: -97.6925576}, FSID: '4cb60ad764998cfa4d6912a2'},
+          {category: '4', title: 'Bartholemews Disc Golf Course', position: {lat: 30.2967083, lng: -97.6895933}, FSID: '4f416feee4b0c868de8c7416'},
+          {category: '5', title: 'MaryMoore Searight Metro Disc Golf Course', position: {lat: 30.2578349, lng: -97.7499692}, FSID: '4d7baad5ea35236a0ad34923'},
+          {category: '5', title: 'Zilker Park Disc Golf Course', position: {lat: 30.2967083, lng: -97.8676655}, FSID: '4bbf8c4274a9a59378a4cef6'}
         ];
 
+var Location = function(data) {
+  this.title = ko.observable(data.title);
+  this.position = ko.observable(data.position);
+  this.category = ko.observable(data.category);
+  this.marker = data.marker;
+};
 
 var map;
 
@@ -23,25 +29,6 @@ function initMap() {
         ko.applyBindings(new ViewModel());
 }
 
-//var Cat = function(data){
-  //this.clickCount = ko.observable(data.clickCount);
-  //this.name = ko.observable(data.name);
-  //this.imgSrc = ko.observable(data.imgSrc);
-  //this.alias = ko.observableArray(data.alias);
-
-  // level up
-  //this.level = ko.computed(function(){
-    //var level;
-    //if (this.clickCount() < 15){
-      //level = 'Newb';
-    //}else if (this.clickCount() < 35){
-      //level = 'Samourai';
-    //}else{
-      //level = 'Kunoichi';
-    //};
-    //return level;
-  //}, this);
-//}
 
 function googleError() {
   alert("Google Maps could not be loaded.");
@@ -72,6 +59,22 @@ var ViewModel = function() {
       setMarkersDefault();
       populateInfoWindow(this, infoWindow);
     });
+
+    //Clicking on marker animates bounce or cancels bounce animation
+    //https://developers.google.com/maps/documentation/javascript/examples/marker-animations
+    locationItem.marker.addListener('click', toggleBounce);
+
+      function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+      }
+
+
+
+    self.locationList.push(new Location(locationItem));
   });
 
   this.currentLocation = ko.observable(this.locationList()[0]);
@@ -93,51 +96,20 @@ var ViewModel = function() {
     }
 
     // Else if a category is selected, filters the list and markers.
-});
-
-function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-          // Clear the infowindow content to give the streetview time to load.
-    infowindow.setContent('');
-    infowindow.marker = marker;
-          // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick', function() {
-      infowindow.marker = null;
+    return ko.utils.arrayFilter(self.locationList(), function(location) {
+      var category = location.category().toLowerCase();
+      var match = category === selectedCategory;
+      self.locationList().forEach(function(location) {
+        if (location.category() === selectedCategory) {
+          location.marker.setVisible(true);
+        } else {
+          location.marker.setVisible(false);
+          infoWindow.close();
+        }
+      });
+      return match;
     });
-    var streetViewService = new google.maps.StreetViewService();
-    var radius = 50;
-
-    //Foursquare implementation
-    var clientID = '1TTRU30VHJFEHTQIAHSEOCJAMFT5AIC0MVYQ54ONFD1UXVEJ';
-    var clientSecret = 'Y1KF2YQECOFMW3CBMWQEZ35FDP1AOFX0S1F2NF3JDJ0FNTXG';
-    var version ='20180417';
-    var venueID = marker.id;
-    var foursquareURL = 'https://api.foursquare.com/v2/venues/'+ venueID +'?&client_id='+ clientID +'&client_secret='+ clientSecret +'&v='+ version;
-
-    $.getJSON(foursquareURL, function(data) {
-      var venueLike = data.response.venue.likes.count;
-      var venueRating = data.response.venue.rating;
-      var fsUrl = data.response.venue.canonicalUrl;
-
-      marker.setIcon(highlightedIcon);
-      infowindow.setContent('<div id="markerTitle">'+ marker.title +'</div><br><div>From Foursquare: <strong>'+ venueLike +'</strong> people have liked this location and it has been rated <strong>'+ venueRating +'</strong>/ 10.</div><br><div><a href="'+ fsUrl +'" target="_blank">Check out this spot on Foursquare</a></div>');
-      infowindow.open(map, marker);
-
-    }).fail(function() {alert('Foursquare could not be loaded...');});
-
-
-  this.currentLocation = function(location) {
-    google.maps.event.trigger(this.marker, 'click');
-  };
-          // In case the status is OK, which means the pano was found, compute the
-          // position of the streetview image, then calculate the heading, then get a
-          // panorama from that and set the options
-
-          // Open the infowindow on the correct marker.
-    infowindow.open(map, marker);
-  }
-}
+});
 
 function makeMarkerIcon(markerColor) {
   var markerImage = new google.maps.MarkerImage(
@@ -155,6 +127,37 @@ function setMarkersDefault() {
     locationItem.marker.setIcon(defaultIcon);
   });
 }
+
+function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+
+    //Foursquare implementation
+    var clientID = '1TTRU30VHJFEHTQIAHSEOCJAMFT5AIC0MVYQ54ONFD1UXVEJ';
+    var clientSecret = 'Y1KF2YQECOFMW3CBMWQEZ35FDP1AOFX0S1F2NF3JDJ0FNTXG';
+    var version ='20180417';
+    var venueID = marker.id;
+    var foursquareURL = 'https://api.foursquare.com/v2/venues/'+ venueID +'?&client_id='+ clientID +'&client_secret='+ clientSecret +'&v='+ version;
+
+
+//Load JSON-encoded data from the server using a GET HTTP Ajaxrequest.
+    $.getJSON(foursquareURL, function(data) {
+      var venueLike = data.response.venue.likes.count;
+      var venueRating = data.response.venue.rating;
+      var fsUrl = data.response.venue.canonicalUrl;
+
+      marker.setIcon(highlightedIcon);
+      infowindow.setContent('<div id="markerTitle">'+ marker.title +'</div><br><div>From Foursquare: <strong>'+ venueLike +'</strong> people have liked this location and it has been rated <strong>'+ venueRating +'</strong>/ 10.</div><br><div><a href="'+ fsUrl +'" target="_blank">Check out this spot on Foursquare</a></div><br><div>(Click icon to reset animation)</div>');
+      infowindow.open(map, marker);
+
+    }).fail(function() {alert('Foursquare could not be loaded...');});
+  }
+
+  this.currentLocation = function(location) {
+    google.maps.event.trigger(this.marker, 'click');
+  };
+
+};
+
   //this.currentLocation = ko.observable( this.locationList()[0] );
   // increment the counter
   //this.incrementCounter = function(){
@@ -165,5 +168,5 @@ function setMarkersDefault() {
   //this.setLocation = function(clickedLocation) {
       //self.currentLocation(clickedLocation);
   //};
-};
+
 
